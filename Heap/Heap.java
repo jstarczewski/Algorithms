4af68d5bd0b4b8a4d9e34e6c2a32696d70383503
jaskola <bartosz.michalowski.j@gmail.com>
2019-01-17 12:49:27 +0100

    Heap done

--- Heap/Heap.java
+++ Heap/Heap.java
@@ -1,75 +1,92 @@
+import java.util.Random;
+
 public class Heap {
 	private int[] heap;
 	private int size;
 	private int end = 0;
 
 	Heap(int size) {
-		this.heap = new int[size];
+		this.heap = new int[size+1];
 		this.size = size;
 	}
 
 	public void add(int value) {
-		this.heap[++end] = value;
-		heapUp(end);
+        if(this.end  > this.size - 1) return;
+		this.heap[++this.end] = value;
+		heapUp(this.heap, this.end);
 	}
 
-    public void heapUp(int index) {
-	int position = index;
-	while( (position / 2) > 0 && heap[position / 2] < heap[position] ){
-            int temp = heap[position/2];
-	    heap[position/2] = heap[position];
-	    heap[position] = temp;
-	    position /= 2;
+    public static void heapUp(int[] v, int index) {
+	while( (index / 2) > 0 && v[index / 2] < v[index] ){
+	    swap(v, index, index / 2);
+        index /= 2;
 		}
 	}
 
-    public boolean delete(int index) {
-        if(index > this.end || index < 1) return false;
-        if(index == this.end) {
+    public void delete() {
+        if(this.end == 1) {
             end--;
-            return true;
+            return;
         }
-        this.heap[index] = this.heap[this.end]; 
-        size--;
-        heapUp(index);
-        return true;
+        this.heap[1] = this.heap[this.end];
+        this.end--;
+        heapDown(this.heap, this.end, 1);
+        return;
+    }
 
-    private void heapDown(int index) {
+    public static void heapDown(int[] v, int size, int index) {
         int child = 2 * index;
-        while(child < this.end){
-            if(child + 1 > this.end && this.heap[child + 1] > this.heap[child])
+        while(child < size){
+            if(child + 1 < size && v[child + 1] > v[child])
                 child++;
-            if(this.heap[child] <= this.heap[index]) return;
-            int tmp = heap[child];
-            heap[child] = heap[index];
-            heap[index] = tmp
+            if(v[child] <= v[index]) return;
+            swap(v, index, child);
             index = child;
             child = index * 2;
         }
     }
 
-    public static void Sort(int[] v) {
-        BuildHeap(v, v.length
+    public static void sort(int[] v) {
+        buildHeap(v, v.length);
         for (int i = v.length - 1; i > 0; i--)
         {
-            Swap(v, 0, i);
-            ShiftDown(v, i, 0);
+            swap(v, 0, i);
+            heapDown(v, i, 0);
         }
     }
 
-    private static void BuildHeap(int[] v, int n) {
+    private static void buildHeap(int[] v, int n) {
         for (int i = n / 2; i >= 0; i--)
-            ShiftDown(v, n, i);
+            heapDown(v, n, i);
 
     }
 
-    private static void Swap(int[] v, int i, int j) {
+    private static void swap(int[] v, int i, int j) {
         int temp = v[i];
         v[i] = v[j];
         v[j] = temp;
     }
 
+    public void showHeap(){
+        for(int i = 1; i <= this.end; i++) {
+            System.out.print(this.heap[i] + ", ");
+        }
+        System.out.println("");
+    }
+
     public static void main(String [] args) throws InterruptedException {
+        Heap heap = new Heap(10);
+        heap.add(1);
+        heap.add(2);
+        heap.add(3);
+        heap.showHeap();
+        heap.add(5);
+        heap.add(4);
+        heap.showHeap();
+        heap.delete();
+        heap.showHeap();
+        heap.delete();
+        heap.showHeap();
         Random r = new Random();
         int n = 50;
         int max = 100;
@@ -77,7 +94,7 @@ public class Heap {
         for(int i = 0; i < n; i++){
             v[i] = r.nextInt() % max;
         }
-        Sort(v);
+        sort(v);
         for(int x : v)
             System.out.print(x + ", ");
     }
